73b19db85094a5cc4d1b50921b61e645e2e0ec44
Farrel Augusta Dinata <farrel.apeiron@gmail.com>
2025-02-21 00:42:00 +0700

    add: simple logic to retrieve data from firestore

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:firebase_core/firebase_core.dart';
+import 'package:flutter_firebase_firestore/screens/home_screen.dart';
 import 'firebase_options.dart';
 
 void main() async {
@@ -16,11 +17,7 @@ class MainApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return const MaterialApp(
-      home: Scaffold(
-        body: Center(
-          child: Text('Hello World!'),
-        ),
-      ),
+      home: HomeScreen(),
     );
   }
 }
---
+++ lib/screens/home_screen.dart
@@ -0,0 +1,137 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_firebase_firestore/repository/user_repository.dart';
+
+class HomeScreen extends StatefulWidget {
+  const HomeScreen({super.key});
+
+  @override
+  State<HomeScreen> createState() => _HomeScreenState();
+}
+
+class _HomeScreenState extends State<HomeScreen> {
+  final UserRepository _userRepository = UserRepository();
+  late Future<List<Map<String, dynamic>>> _userData;
+
+  @override
+  void initState() {
+    super.initState();
+    setState(() {
+      _userData = Future.value([]);
+    });
+  }
+
+  _getUserData() {
+    setState(() {
+      _userData = _userRepository.getAllUsers();
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Flutter Firebase Firestore Demo"),
+        centerTitle: true,
+      ),
+      body: Center(
+        child: Padding(
+          padding: EdgeInsets.all(20),
+          child: Column(
+            children: [
+              ElevatedButton(
+                  onPressed: _getUserData,
+                  child: Text("Get data from Firestore!")),
+              SizedBox(height: 50),
+              FutureBuilder(
+                  future: _userData,
+                  builder: (context, snapshot) {
+                    if (snapshot.connectionState == ConnectionState.waiting) {
+                      return Center(child: CircularProgressIndicator());
+                    } else if (snapshot.hasError) {
+                      return Center(child: Text("Error: ${snapshot.error}"));
+                    } else if (!snapshot.hasData || snapshot.data!.isEmpty) {
+                      return Center(child: Text("No users found"));
+                    }
+
+                    List<Map<String, dynamic>> users = snapshot.data!;
+
+                    return _userContent(users);
+                  })
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+
+  Widget _userContent(List<Map<String, dynamic>> users) {
+    return Expanded(
+        child: ListView.builder(
+      itemCount: users.length,
+      itemBuilder: (context, index) {
+        var user = users[index];
+        return Card(
+          margin: EdgeInsets.symmetric(vertical: 8),
+          elevation: 4,
+          shape: RoundedRectangleBorder(
+            borderRadius: BorderRadius.circular(12),
+          ),
+          child: Padding(
+            padding: EdgeInsets.all(16),
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                Text(
+                  user["name"] ?? "No Name",
+                  style: TextStyle(
+                    fontSize: 20,
+                    fontWeight: FontWeight.bold,
+                  ),
+                ),
+                SizedBox(height: 8),
+                Row(
+                  children: [
+                    Icon(Icons.cake, size: 20, color: Colors.grey),
+                    SizedBox(width: 8),
+                    Text(
+                      "Age: ${user["age"] ?? "Unknown"}",
+                      style: TextStyle(fontSize: 16),
+                    ),
+                  ],
+                ),
+                SizedBox(height: 8),
+                Row(
+                  children: [
+                    Icon(Icons.interests, size: 20, color: Colors.grey),
+                    SizedBox(width: 8),
+                    Text(
+                      "Hobby: ${user["hobby"] ?? "Not specified"}",
+                      style: TextStyle(fontSize: 16),
+                    ),
+                  ],
+                ),
+                SizedBox(height: 8),
+                Row(
+                  children: [
+                    Icon(
+                      user["gender"] == "male" 
+                      ? Icons.male 
+                      : Icons.female, 
+                      size: 20, 
+                      color: Colors.grey
+                    ),
+                    SizedBox(width: 8),
+                    Text(
+                      "Gender: ${user["gender"] ?? "Not specified"}",
+                      style: TextStyle(fontSize: 16),
+                    ),
+                  ],
+                ),
+              ],
+            ),
+          ),
+        );
+      },
+    ));
+  }
+}
